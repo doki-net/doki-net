faeb244eae96b3050b96a62f9dfa1f5f171bebdb
Olly Fallows <ollyfallows@outlook.com>
2018-10-02 22:42:27 +0100

    Mutation and structure are looking promising

--- com/ngumi/monika/bin/app.js
+++ com/ngumi/monika/bin/app.js
@@ -1,6 +1,10 @@
 const FS = require("fs");
+const UTIL = require('../lib/Util');
 const RNN = require('../lib/RNN');
-var baseNet = '{"nodes":[[{"bias":0, "a":0}],[{"bias":0, "a":0}]],"edges":[],"mutationRate":0.9}';
 
-var pop = new RNN(baseNet);
-console.log(pop);
+var baseNet = JSON.parse(FS.readFileSync('../cfg/exampleRNN.json'));
+
+baseNet = UTIL.mutate(baseNet);
+//console.log(pop.run([[0],[1],[0],[0],[0]]));
+//console.log(pop.run([[0],[1],[0],[0],[0]]));
+console.log(baseNet);
